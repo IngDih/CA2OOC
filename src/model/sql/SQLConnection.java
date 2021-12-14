fbb7ac539ecd2836ffcaac61ef578f97cf7422f1
Robert Szlufik <robert.szlufik123@gmail.com>
2021-12-14 20:17:06 +0000

    methods to implement in connection class

--- src/model/sql/SQLConnection.java
+++ src/model/sql/SQLConnection.java
@@ -4,10 +4,35 @@
  */
 package model.sql;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author rober
  */
 public class SQLConnection {
+    
+    
+    
+    
+    
+    
+    
+    // to implement 
+    
+    
+    public String deleteUser(int user_id){};
+    
+    
+    
+    public Map<String,List<String>> getAllOperations(){}
+    
+    
+     public Map<String, String> fetchAllUsers(){};
+     
+     public Map<String, String> fetchUser(int user_id){};
+     
+      public boolean saveSolution(int user_id , String equations, String x, String y, String z){};
     
 }
