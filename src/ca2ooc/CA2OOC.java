9267e30383eaf55499dc422865ca02a702ab78f7
Ingrid Castro <72034663+IngDih@users.noreply.github.com>
2021-12-18 00:53:22 +0000

    ca2ooc

--- src/ca2ooc/CA2OOC.java
+++ src/ca2ooc/CA2OOC.java
@@ -8,12 +8,17 @@ package ca2ooc;
 import views.LoginForm;
 /**
  *
- * @author Robert Szlufik #2020358 Ingrid Castro #2020341
+ * @author Robert Szlufik #2020358 
+ *         Ingrid Castro #2020341
  */
 public class CA2OOC {
 
     /**
      * @param args the command line arguments
+     * Link for the public GITHUB repository: https://github.com/IngDih/CA2OOC
+     * Change password if needed on SQLConnection.java according to your localhost one.
+     * If database doesn't run, check if the mysql-connector-java-8.0.26.jar is in libraries. 
+     * If not you can add .JAR folder that is included on the ca2ooc packet.
      */
     public static void main(String[] args) {
         LoginForm loginScreen = new LoginForm();
